97ade29e0d0f07522f3df55b1129869e275c3b86
molly-yun <101503850+molly-yun@users.noreply.github.com>
2022-03-22 23:14:05 +0900

    [Bronze IV] Title: 두 수 비교하기, Time: 204 ms, Memory: 17704 KB -BaekjoonHub

---
+++ 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a,b;
+        
+        a = scanner.nextInt();
+        b = scanner.nextInt();
+        
+        scanner.close();
+        
+        if(a>b) {
+            System.out.println(">");
+        } else if(a<b) {
+            System.out.println("<");
+        } else {
+            System.out.println("==");
+        }
+    }
+}
